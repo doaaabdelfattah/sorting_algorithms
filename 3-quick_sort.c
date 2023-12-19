dcc398fc06cc4c4e875bd364652a38e66a196a2d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-12-19 11:31:36 +0000

    “fix_errors”

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -22,50 +22,50 @@ void swap_ints(int *a, int *b)
  */
 int partition(int *array, size_t size, int low, int high)
 {
-	int pivot, j, i, swap;
-	swap = 0;
+	int pivot, j, i;
 
 	/* Choose the last element as the pivot*/
 	pivot = array[high];
-	i = low - 1;
+	i = low - 1 ; /* Index of Temporary pivot index */
 	for (j = low; j <= high - 1; j++)
+	{
+		/* If the current element is less than or equal to the pivot*/
+		if (array[j] < pivot)
 		{
-			if (array[j] <= pivot)
+		/* Move the temporary pivot index forward */
+		i++;
+		/* swap the current element with the element at the temporary pivot index*/
+		if (array[i] != array[j])
 			{
-				i++;
-				swap_ints(&array[i], &array[j]);
-				swap = 1;
-				/* print_array(array, size);*/
+			swap_ints(&array[i], &array[j]);
+			print_array(array, size);
 			}
 		}
-	if (array[i + 1] > pivot)
-	{
-		swap_ints(&array[i + 1], &array[high]);
-		swap = 1;
-		/*print_array(array, size);*/
 	}
-	if (swap)
-		print_array(array, size);
+	/* Move the pivot element to the correct pivot position */
+	/* (between the smaller and larger elements) */
+	swap_ints(&array[i + 1], &array[high]);
+	print_array(array, size);
 	return (i + 1);
 }
 /**
  * lumoto_sort - quicksort algorithm through recursion.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- * @left: The starting index of the array partition to order.
- * @right: The ending index of the array partition to order.
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
+ * @low: Left side
+ * @high: Right side
 */
-void lumoto_sort(int *array, size_t size, int left, int right)
+void lumoto_sort(int *array, size_t size, int low, int high)
 {
 	int location;
 
-	if (right - left > 0)
+	if (high - low > 0)
 	{
-		/* locationition array and get the pivot index */
-		location = partition(array, size, left, right);
+		/* partition the array and get the pivot index */
+		location = partition(array, size, low, high);
 		/* Sort the two partition */
-		lumoto_sort(array, size, left, location - 1);
-		lumoto_sort(array, size, location + 1, right);
+		lumoto_sort(array, size, low, location - 1);
+		lumoto_sort(array, size, location + 1, high);
 	}
 }
 /**
